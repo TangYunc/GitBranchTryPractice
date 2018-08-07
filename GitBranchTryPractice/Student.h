244ba6f05fab043860deacda135b197cf5dc66ba
yunchuan <tangyc@zgjzd.cn>
2018-08-07 16:40:20 +0800

    222

---
+++ GitBranchTryPractice/Student.h
@@ -0,0 +1,13 @@
+//
+//  Student.h
+//  GitBranchTryPractice
+//
+//  Created by tangyunchuan on 2018/8/7.
+//  Copyright © 2018年 tangyunchuan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Student : NSObject
+
+@end
---
+++ GitBranchTryPractice/Student.m
@@ -0,0 +1,13 @@
+//
+//  Student.m
+//  GitBranchTryPractice
+//
+//  Created by tangyunchuan on 2018/8/7.
+//  Copyright © 2018年 tangyunchuan. All rights reserved.
+//
+
+#import "Student.h"
+
+@implementation Student
+
+@end
